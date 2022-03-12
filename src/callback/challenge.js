0f36f6259f054cbaf4615e93467395e3249eb8b6
Omar Terrazas <terrazas.omar@gmail.com>
2022-03-11 18:18:38 -0600

    Múltiples Peticiones a un API con Callbacks

--- src/callback/challenge.js
+++ src/callback/challenge.js
@@ -1,10 +1,11 @@
 let XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest;
+let API = 'https://rickandmortyapi.com/api/character/';
 
 function fetchData(url_api, callback) {
     let xhttp = new XMLHttpRequest();
-    
+
     xhttp.open('GET', url_api, true);
-    xhttp.onreadystatechange = function(event) {
+    xhttp.onreadystatechange = function (event) {
         /**
          * 
          * Holds the status of the XMLHttpRequest.
@@ -14,8 +15,8 @@ function fetchData(url_api, callback) {
          * 3: processing request
          * 4: request finished and response is ready
          * 
-        **/
-        if(xhttp.readyState === 4) {
+         **/
+        if (xhttp.readyState === 4) {
             /**
              * 
              * Http Messages Reference
@@ -23,8 +24,8 @@ function fetchData(url_api, callback) {
              * 403: "Forbidden"
              * 404: "Page not found"
              * 
-            **/
-            if(xhttp.status === 200) {
+             **/
+            if (xhttp.status === 200) {
                 callback(null, JSON.parse(xhttp.responseText));
             } else {
                 const error = new Error('Error ' + url_api);
@@ -34,3 +35,16 @@ function fetchData(url_api, callback) {
     }
     xhttp.send();
 }
+
+fetchData(API, function (error1, data1) {
+    if (error1) return console.error(error1);
+    fetchData(API + data1.results[0].id, function (error2, data2) {
+        if (error2) return console.error(error2);
+        fetchData(data2.origin.url, function (error3, data3) {
+            if(error3) return console.error(error3);
+            console.log(data1.info.count);
+            console.log(data2.name);
+            console.log(data3.dimension);
+        });
+    });
+});
